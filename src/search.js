be906eee13de94e94cf4a1cbf10a3636a0533d96
Ziva Xu <yxxuziva@gmail.com>
2018-08-09 10:59:29 +0800

    Styled search function

--- src/search.js
+++ src/search.js
@@ -1,35 +1,9 @@
-// function() {
-//   const addActive = (event) => {
-  // };
-// const addActive = (event) => {
-// document.getElementsByClassName("nav-search")[0].classList.add("active");
-// }
-// document.getElementById("search-input").addEventListener("focus", addActive);
-
-   //  $('#search-input').on('keyup', function() {
-   //    $('#hits-container').scrollTop(0);
-   //  })
-
-   //  $('.close-search').on('click', function(evt) {
-   //    evt.preventDefault();
-   //    $('#search-input').val('');
-   //     $('.nav-search').removeClass('active');
-   //   })
-
-   // $('#search-input').on('blur', function(evt) {
-   //   if(!evt.isDefaultPrevented) {
-   //     console.log("blur");
-   //    $('.nav-search').removeClass('active');
-   //   }
-   //  })
-// };
-
 // TODO: Autocomplete the input with AJAX calls.
 const displayResults = (companyList) => {
   const list = document.getElementById("results");
   if (companyList) {
     for (let i = 0; i < companyList.length; i += 1) {
-      list.insertAdjacentHTML("beforeend", `<li>${companyList[i].name}${companyList[i].ticker}</li>`);
+      list.insertAdjacentHTML("beforeend", `<div class="hit"><div class="hit-title">${companyList[i].name}</div><div class="hit-description">${companyList[i].ticker}</div></div>`);
     }
   } else {
     list.insertAdjacentHTML("beforeend", `<li>Sorry, the company cannot be found.</li>`);
@@ -68,8 +42,14 @@ const input = document.getElementById("input-content");
 input.addEventListener("keyup", searchWords);
 
 
-// function toTitleCase(str) {
-//     return str.replace(/(?:^|\s)\w/g, function(match) {
-//         return match.toUpperCase();
-//     });
-// }
+const clearFields = (event) => {
+  document.getElementById('input-content').value = '';
+  const myNode = document.getElementById("results");
+  while (myNode.firstChild) {
+    myNode.removeChild(myNode.firstChild);
+  }
+  document.getElementsByClassName("nav-search")[0].classList.remove("active");
+}
+
+const closeSearch = document.getElementById("close-search-icon");
+closeSearch.addEventListener("click", clearFields);
