6172589fa32e9d2b5814deee6a24af60f9e64ccc
Jiarui Yan <raymondyjr1120@gmail.com>
2018-08-10 09:49:30 +0800

    leancloud everything

--- src/readfromdb.js
+++ src/readfromdb.js
@@ -1,10 +1,21 @@
 const { Query, User } = AV;
 AV.init('HS8Deh8YMUw4Vy6Fl3ixLQNN-gzGzoHsz', 'vCCT2XtjqFeBaTgdPMHTxbbu');
 
-// let year =
-const query = new AV.Query('Balance_Sheet')
-  .equalTo('FY', 2017)
-  .equalTo('Ticker', 'AAPL')
-  .find()
-  .then(res => document.write(res[0].attributes))
+const ticker = getUrlParams(window.location.search).ticker;
+let year = [2013, 2014, 2015, 2016, 2017];
+const row_name = ['cashandequivalents', 'shortterminvestments', 'notereceivable', 'accountsreceivable', 'netinventory', 'othercurrentassets', 'totalcurrentassets', 'netppe', 'longterminvestments', 'goodwill', 'intangibleassets', 'othernoncurrentassets', 'totalnoncurrentassets', 'totalassets', 'shorttermdebt', 'accountspayable', 'accruedexpenses', 'totalcurrentliabilities', 'longtermdebt', 'othernoncurrentliabilities', 'totalnoncurrentliabilities', 'totalliabilities', 'commitmentsandcontingencies', 'commonequity', 'retainedearnings', 'aoci', 'totalcommonequity', 'totalequity', 'totalequityandnoncontrollinginterests', 'totalliabilitiesandequity', 'currentdeferredrevenue', 'noncurrentdeferredrevenue']
 
+for (const y of year) {
+  for (const item of row_name) {
+    const query = new AV.Query('Balance_Sheet')
+      .equalTo('FY', y)
+      .equalTo('Ticker', ticker)
+      .select([item])
+      .find()
+      .then(res => {
+        console.log(res[0].attributes);
+        const output = document.getElementById(item+`${y}`);
+        output.innerHTML = res[0].attributes[item];
+      });
+  };
+};
