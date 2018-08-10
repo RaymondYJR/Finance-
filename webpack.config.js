764bc82c3a26ca38b9ab4dc1df585638416743b8
Ziva Xu <yxxuziva@gmail.com>
2018-08-10 17:23:21 +0800

    webpack -p builds to dist, JS in dist/build

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   entry: "./src/browser.js",
   mode: "development",
   output: {
-    path: __dirname + "/build",
-    filename: "application.js"
+    path: __dirname + "/dist",
+    filename: "build/application.js"
    },
   devtool: "sourcemap",
   devServer: {
     publicPath: "/",
     historyApiFallback: true
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({  // Also generate a test.html
+      filename: 'dashboard.html',
+      template: 'dashboard.html'
+    })
+  ]
 };
