7d21f0613dd3e14c449c87c7efaf768e07495374
Ziva Xu <yxxuziva@gmail.com>
2018-08-12 23:00:52 +0800

    finished dashboard registration/login/logout header, fixed positioning of it in dashboard

--- src/registration_popup.js
+++ src/registration_popup.js
@@ -1,17 +1,19 @@
 function div_show() {
   document.getElementById('registration-popup').style.display = "block";
-  document.getElementById("overlay").style.display = "block";
+  document.getElementsByClassName("overlay")[0].style.display = "block";
 }
 
 function div_hide(){
   document.getElementById('registration-popup').style.display = "none";
-  document.getElementById("overlay").style.display = "none";
+  document.getElementsByClassName("overlay")[0].style.display = "none";
 }
 
 function div_show_login() {
   document.getElementById('login-popup').style.display = "block";
+  document.getElementsByClassName("overlay")[0].style.display = "block";
 }
 
 function div_hide_login(){
   document.getElementById('login-popup').style.display = "none";
+  document.getElementsByClassName("overlay")[0].style.display = "none";
 }
