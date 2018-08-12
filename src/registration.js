ea7bdfc4e14be4db977572efb4e71284990448bf
Ziva Xu <yxxuziva@gmail.com>
2018-08-12 20:48:33 +0800

    finished user reg

--- src/registration.js
+++ src/registration.js
@@ -1,24 +1,33 @@
 let reg = document.getElementById('registration-form');
 
 const register = (event) => {
-  console.log('yea!');
+  event.preventDefault();
   let user = new AV.User();
-  user.setUsername(document.getElementById("reg-username-input").value);
-  user.setPassword(document.getElementById("reg-password-input").value);
-  user.setEmail(document.getElementById("reg-email-input").value);
-  user.signUp().then(function (loggedInUser) {
+  let username = document.getElementById("reg-username-input").value;
+  let password = document.getElementById("reg-password-input").value;
+  let email = document.getElementById("reg-email-input").value;
+  user.setUsername(username);
+  user.setPassword(password);
+  user.setEmail(email);
+  if (username === "") {
+    alert("请填写用户名");
+  } else if (email === "") {
+    alert("请填写邮箱");
+  } else if (password === "") {
+    alert("请填写密码");
+  } else {
+    user.signUp().then(function (loggedInUser) {
       console.log(loggedInUser);
-  }, function (error) {
-  });
+    }, function (error) {
+      let errorMes = error.message.match(/.+\u3002/)[0];
+      if (errorMes != null) {
+        alert(errorMes);
+      }
+    });
+  }
 }
 
 if (reg) {
   const submit = document.getElementById('reg-submit');
-  input.addEventListener("submit", register);
+  submit.addEventListener("click", register);
 }
-
-const give = (event) => {
-  console.log('read!');
-}
-
-document.getElementById('test').addEventListener("click", give);
