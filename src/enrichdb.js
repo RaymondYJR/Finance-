17b54c52b9c0622bc3f0b526cc75425b16984bdb
Jiarui Yan <raymondyjr1120@gmail.com>
2018-08-14 10:31:23 +0800

    finished enrich company into db

--- src/enrichdb.js
+++ src/enrichdb.js
@@ -10,31 +10,40 @@ function getUrlParams(search) {
 const ticker = getUrlParams(window.location.search).ticker;
 let year = [2013, 2014, 2015, 2016, 2017];
 
-// var Company = AV.Object.extend('Company');
-// var company = new Company();
-// company.set('Ticker', ticker);
-// company.save();
-
 let headers = new Headers();
 headers.set('Authorization', 'Basic ' + 'ZjNkNmRkMTcxNjNmNGZmMjU2OGE4YWE1ZjNiMTU1YmM6NzQ0OGNiNjY1YTBkYmNjMGNjMjdjNzBlZGRhMGRlZTE=');
 
-const Balance_Sheet = AV.Object.extend('Balance_Sheet');
+// const Company = AV.Object.extend('Company');
+// const Balance_Sheet = AV.Object.extend('Balance_Sheet');
 // const Income_Statement = AV.Object.extend('Income_Statement');
 // const Cash_Flow_Statement = AV.Object.extend('Cash_Flow_Statement');
 
-for (const y of year) {
-  let url_bs = `https://api.intrinio.com/financials/standardized?identifier=${ticker}&statement=balance_sheet&type=FY&fiscal_year=${y}`;
-  fetch(url_bs, {method:'GET', headers: headers})
-    .then(response => response.json())
-    .then((data) => {
-      const balance_sheet = new Balance_Sheet();
-      balance_sheet.set('FY', y);
-      balance_sheet.set('Ticker', ticker);
-      for (let i = 0; i < (data["data"].length); i++) {
-        balance_sheet.set(data["data"][i]["tag"], data["data"][i]["value"]);
-      };
-      balance_sheet.save();
-    });
+let url_comp = `https://api.intrinio.com/companies?identifier=${ticker}`;
+fetch(url_comp, {method:'GET', headers: headers})
+  .then(response => response.json())
+  .then((data) => {
+    const company = new Company();
+    company.set('Ticker', ticker);
+    company.set('Name', data["name"]);
+    company.set('Company_url', data["Company_url"]);
+    company.set('Industry_group', data["industry_group"]);
+    company.set('Short_description', data["short_description"]);
+    company.save();
+  });
+
+// for (const y of year) {
+//   let url_bs = `https://api.intrinio.com/financials/standardized?identifier=${ticker}&statement=balance_sheet&type=FY&fiscal_year=${y}`;
+//   fetch(url_bs, {method:'GET', headers: headers})
+//     .then(response => response.json())
+//     .then((data) => {
+//       const balance_sheet = new Balance_Sheet();
+//       balance_sheet.set('FY', y);
+//       balance_sheet.set('Ticker', ticker);
+//       for (let i = 0; i < (data["data"].length); i++) {
+//         balance_sheet.set(data["data"][i]["tag"], data["data"][i]["value"]);
+//       };
+//       balance_sheet.save();
+//     });
 
   // let url_is = `https://api.intrinio.com/financials/standardized?identifier=${ticker}&statement=income_statement&type=FY&fiscal_year=${y}`;
   // fetch(url_is, {method:'GET', headers: headers})
@@ -61,4 +70,4 @@ for (const y of year) {
   //     };
   //     cash_flow_statement.save();
   //   });
-};
+// };
