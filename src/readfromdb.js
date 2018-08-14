898645fc1c3e6acf0d83757ec8c2e2b3ec7408a3
Winfield Fan <faefaehk@gmail.com>
2018-08-14 21:51:47 +0800

    please work

--- src/readfromdb.js
+++ src/readfromdb.js
@@ -32,10 +32,16 @@ function getHead() {
   const tablerows = document.getElementsByClassName('graphhelper');
   for (let k = 1; k < tablerows.length; k++) {
     const tablerow = document.getElementsByClassName('graphhelper')[k].textContent.split("\n").join("").split(" ").filter(item => item !== "");
-    result[tablerow[0]] = tablerow.slice(1);
+    let tablerow_data = [];
+    tablerow.slice(1).forEach(function(num){tablerow_data.push(Number(num))});
+    result[tablerow[0]] = tablerow_data;
   };
   return result;
 };
 
-console.log(getHead());
+setTimeout(getHead, 4000)
+attempt = document.getElementsByClassName('please')
+console.log(getHead())
+attempt.innerHTML = `<p>{getHead()}<p>`
+
 
