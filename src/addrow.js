96cf0202876eea2229c30dc444ed60eb256e4cf3
Jiarui Yan <raymondyjr1120@gmail.com>
2018-08-14 13:21:55 +0800

    fuck

--- src/addrow.js
+++ src/addrow.js
@@ -3,7 +3,9 @@
 //   const tablerows = document.getElementsByClassName('graphhelper');
 //   for (let k = 1; k < tablerows.length; k++) {
 //     const tablerow = document.getElementsByClassName('graphhelper')[k].textContent.split("\n").join("").split(" ").filter(item => item !== "");
-//     result[tablerow[0]] = tablerow.slice(1);
+//     let tablerow_data = [];
+//     tablerow.slice(1).forEach(function(num){tablerow_data.push(Number(num))});
+//     result[tablerow[0]] = tablerow_data;
 //   };
 //   return result;
 // };
--- src/readfromdb.js
+++ src/readfromdb.js
@@ -22,17 +22,20 @@ for (const y of year) {
         const output = document.getElementById(item+`${y}`);
         output.innerHTML = (financials[item] / 100000000).toFixed(2);
       };
-    });
+    })
 };
+
+
 
 function getHead() {
   result = {};
   const tablerows = document.getElementsByClassName('graphhelper');
   for (let k = 1; k < tablerows.length; k++) {
     const tablerow = document.getElementsByClassName('graphhelper')[k].textContent.split("\n").join("").split(" ").filter(item => item !== "");
-    result[tablerow[0]] = tablerow.slice(1);
+    let tablerow_data = [];
+    tablerow.slice(1).forEach(function(num){tablerow_data.push(Number(num))});
+    result[tablerow[0]] = tablerow_data;
   };
   return result;
 };
 
-console.log(getHead());
