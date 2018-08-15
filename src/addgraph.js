ad69a1cabb21a96b0ae19ebe897ed607370ac6e2
Jiarui Yan <raymondyjr1120@gmail.com>
2018-08-15 19:52:25 +0800

    finished graphing

--- src/addgraph.js
+++ src/addgraph.js
@@ -1,9 +1,65 @@
-// function graphCreateFunction(str,arr) {
-//   const table = document.getElementById("user-designed-data");
-//   const graph = table.insertgraph(0);
-//   cell1.innerHTML = str;
-//   cell2.innerHTML = arr[0];
-// }
+function graphCreateBar(str, arr) {
+  const losefield = document.getElementById(str).getContext('2d');
+  let newBarChart = new Chart(losefield, {
+    type: 'bar',
+    data: {
+      labels: ["2013", "2014", "2015", "2016", "2017"],
+      datasets: [{
+        label: `# ${str}`,
+        data: arr,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {scales: {yAxes: [{ticks: {beginAtZero:true}}]}}
+  });
+};
+
+// function graphCreatePie(str, arr) {
+//   const plosefield = document.getElementById("default-pieChart").getContext('2d');
+//   let newPieChart = new Chart(plosefield, {
+//     type: 'pie',
+//     data: {
+//       labels: [, , , , ],
+//       datasets: [{
+//         data: arr
+//       }]
+//     }
+//   });
+// };
+
+function graphCreateLine(str, arr) {
+  const llosefield = document.getElementById(str).getContext('2d');
+  let newLineChart = new Chart(llosefield, {
+    type: 'line',
+    data: {
+      labels: ["2013", "2014", "2015", "2016", "2017"],
+      datasets: [{
+        label: `# ${str}`,
+        data: arr,
+      }]
+    },
+    options: {scales: {yAxes: [{ticks: {beginAtZero:true}}]}}
+  });
+};
+
+function addCanvasToHtml(str) {
+  const container_block = document.getElementById('graphs-row');
+  container_block.insertAdjacentHTML("beforeend", `<div class="col-md-6"><div class="graph-card"><canvas id="${str}" width="200" height="200"></canvas></div></div>`);
+};
 
 let add_graph_form = document.getElementById('addgraph-form');
 
@@ -11,13 +67,13 @@ const addgraph = (event) => {
   event.preventDefault();
   let newgraphname = document.getElementById("addgraph-graphname-input").value;
   let datasetgraphname = document.getElementById("data-select-graph").value;
-  const datasetonedata = getHead()[datasetgraphname];
+  const datasetgraphdata = getHead()[datasetgraphname];
 
   let barstatus = document.getElementById("addgraph-bar").checked;
   let piestatus = document.getElementById("addgraph-pie").checked;
   let linestatus = document.getElementById("addgraph-line").checked;
 
-  let addgraphdata;
+  let graphtype;
 
   if (newgraphname === "") {
     alert("请填写图表名称");
@@ -27,13 +83,15 @@ const addgraph = (event) => {
     alert("请选择一种图表类型");
   } else {
     if (barstatus === true) {
-
+      addCanvasToHtml(`${newgraphname}`);
+      graphCreateBar(`${newgraphname}`, datasetgraphdata);
     } else if (piestatus === true) {
-
+      addCanvasToHtml(`${newgraphname}`);
+      graphCreatePie(datasetgraphdata);
     } else {
-
+      addCanvasToHtml(`${newgraphname}`);
+      graphCreateLine(`${newgraphname}`, datasetgraphdata);
     };
-    // graphCreateFunction(`${newgraphname}`, addgraphdata);
     div_hide_addgraph();
   };
 }
--- src/graph.js
+++ src/graph.js
@@ -1,70 +1,69 @@
-Chart.defaults.global.defaultFontSize = 15 ;
-
-console.log(getHead()['资产总计']);
+Chart.defaults.global.defaultFontSize = 15;
 
 const ctx = document.getElementById("default-barchart").getContext('2d');
 let myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["2013", "2014", "2015", "2016", "2017"],
-        datasets: [{
-            label: '# of Votes',
-            data: [2070, 2318.39, 2903.45, 3216.86, 3753.19],//getHead()['资产总计'],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
+  type: 'bar',
+  data: {
+    labels: ["2013", "2014", "2015", "2016", "2017"],
+    datasets: [{
+      label: '# of Votes',
+      data: [1,2,3,4,5],//getHead()['资产总计'],
+      // backgroundColor: [
+      //   'rgba(255, 99, 132, 0.2)',
+      //   'rgba(54, 162, 235, 0.2)',
+      //   'rgba(255, 206, 86, 0.2)',
+      //   'rgba(75, 192, 192, 0.2)',
+      //   'rgba(153, 102, 255, 0.2)'
+      // ],
+      // borderColor: [
+      //   'rgba(255,99,132,1)',
+      //   'rgba(54, 162, 235, 1)',
+      //   'rgba(255, 206, 86, 1)',
+      //   'rgba(75, 192, 192, 1)',
+      //   'rgba(153, 102, 255, 1)'
+      // ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero:true
         }
+      }]
     }
+  }
 });
 
 const lctx = document.getElementById("default-lineChart").getContext('2d');
 let lineChart = new Chart(lctx, {
-    type: 'line',
-    data: {
-      labels: ["2013", "2014", "2015", "2016", "2017"],
-      datasets: [{
-        data: [20, 10, 30, 48, 8]
+  type: 'line',
+  data: {
+    labels: ["2013", "2014", "2015", "2016", "2017"],
+    datasets: [{
+      data: [1,2,3,4,5]//getHead()['负债总计']
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero:true
+        }
       }]
-    },
-    options: {
-      scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero:true
-                  }
-              }]
-          }
     }
+  }
 });
 
-const pctx = document.getElementById("default-pieChart").getContext('2d');
-let pieChart = new Chart(pctx, {
-    type: 'pie',
-    data: {
-      labels: ["2013", "2014", "2015", "2016", "2017"],
-      datasets: [{
-        data: [20, 10, 30, 48, 8]
-      }]
-    }
-});
+// const pctx = document.getElementById("default-pieChart").getContext('2d');
+// let pieChart = new Chart(pctx, {
+//   type: 'pie',
+//   data: {
+//     labels: [, , , , ],
+//     datasets: [{
+//       data: [20, 10, 30, 48, 8]
+//     }]
+//   }
+// });
+
