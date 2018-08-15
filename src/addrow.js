511139f5e98818923a39d11b228f3f428720b6fd
Winfield Fan <faefaehk@gmail.com>
2018-08-15 19:51:49 +0800

    raymondcantcss

--- src/addrow.js
+++ src/addrow.js
@@ -45,19 +45,22 @@ function divide(arr1, arr2) {
 function rowCreateFunction(str,arr) {
   const table = document.getElementById("user-designed-data");
   const row = table.insertRow(0);
-  const cell1 = row.insertCell(0);
+  const cell1 = document.createElement('th');
+  row.appendChild(cell1)
   const cell2 = row.insertCell(1);
   const cell3 = row.insertCell(2);
   const cell4 = row.insertCell(3);
   const cell5 = row.insertCell(4);
   const cell6 = row.insertCell(5);
-  row.classList.add("parent");
+  row.classList.add("parent", "graphhelper");
   cell1.innerHTML = str;
   cell2.innerHTML = arr[0];
   cell3.innerHTML = arr[1];
   cell4.innerHTML = arr[2];
   cell5.innerHTML = arr[3];
   cell6.innerHTML = arr[4];
+  cell1.classList.add("first-show", "i-hate-raymond");
+  cell1.setAttribute('scope', 'row');
 }
 
 let add_row_form = document.getElementById('addrow-form');
